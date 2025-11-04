27577cd3eaa91072797ed433aac6cecfc0977697
Giacomo Marciani <mgiacomo@amazon.com>
2025-11-04 17:47:51 -0500

    [Dependencies] Upgrade Python runtime to Python 3.12 in all Lambda functions.

--- cli/src/pcluster/templates/cdk_builder_utils.py
+++ cli/src/pcluster/templates/cdk_builder_utils.py
@@ -972,7 +972,7 @@ class PclusterLambdaConstruct(Construct):
             handler=f"{handler_func}.handler",
             memory_size=128,
             role=execution_role,
-            runtime="python3.9",
+            runtime="python3.12",
             timeout=timeout,
             vpc_config=awslambda.CfnFunction.VpcConfigProperty(
                 security_group_ids=config.lambda_functions_vpc_config.security_group_ids,
